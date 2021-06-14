a581e1e648b40e3cc19b9a4adee08754590ff6a6
codebuff2020 <codebuff2020@gmail.com>
2021-06-14 17:37:59 +0530

    Base entity added for ID - future repository purpose

--- sam-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
+++ sam-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -1,6 +1,6 @@
-package guru.springframework.model;
+package guru.springframework.sfgpetclinic.model;
 
-public class Person {
+public class Person extends BaseEntity {
 
   private String firstName;
   private String lastName;
--- sam-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ sam-pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,6 +1,6 @@
-package guru.springframework.model;
+package guru.springframework.sfgpetclinic.model;
 
-public class PetType {
+public class PetType extends BaseEntity {
  private String name;
 
   public String getName() {
